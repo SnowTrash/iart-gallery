fb7eba2672a8f59c4461f3c57b5b95a859fdfdb7
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2023-10-26 15:08:38 -0600

    cargando
    se agrega una pantalla de carga para evitar el bug de modo creativo

--- src/index.js
+++ src/index.js
@@ -17,75 +17,121 @@ var fovY = () => 2 * Math.atan(Math.tan(fovX() * 0.5) * window.innerHeight / win
 const Stats = require('stats.js');
 var stats = new Stats();
 stats.showPanel(0);
-if(showStats) {
+if (showStats) {
 	document.body.appendChild( stats.dom );
 }
 
 let regl, map, drawMap, placement, drawPainting, fps;
 
-regl = require('regl')({
-	extensions: [
-		//'angle_instanced_arrays',
-		'OES_element_index_uint',
-		'OES_standard_derivatives'
-	],
-	optionalExtensions: [
-		//'oes_texture_float',
-		'EXT_texture_filter_anisotropic'
-	],
-	attributes: { alpha : false },
-});
+// Agregar una pantalla de carga
+const loadingScreen = document.createElement('div');
+loadingScreen.style.position = 'absolute';
+loadingScreen.style.top = '50%';
+loadingScreen.style.left = '50%';
+loadingScreen.style.transform = 'translate(-50%, -50%)'; // Centrar vertical y horizontalmente
+loadingScreen.style.width = '100%';
+loadingScreen.style.height = '100%';
+loadingScreen.style.backgroundColor = 'gray'; // Puedes personalizar el color de fondo
+loadingScreen.style.display = 'flex';
+loadingScreen.style.justifyContent = 'center';
+loadingScreen.style.alignItems = 'center';
+loadingScreen.style.zIndex = '9999'; // Asegura que la pantalla de carga esté por encima de todo
+loadingScreen.innerHTML = 'Cargando';
+document.body.appendChild(loadingScreen);
 
-map = require('./map')();
-const mesh = require('./mesh');
-drawMap = mesh(regl, map, useReflexion);
-placement = require('./placement')(regl, map);
-drawPainting = require('./painting')(regl);
-fps = require('./fps')(map, fovY);
+// Textos para la animación de carga
+const loadingTexts = ['Cargando', 'Cargando.', 'Cargando..', 'Cargando...'];
+let currentTextIndex = 0;
 
-const context = regl({
-	cull: {
-		enable: true,
-		face: 'back'
-	},
-	uniforms: {
-		view: fps.view,
-		proj: fps.proj,
-		yScale: 1.0
-	}
-});
+const updateLoadingText = () => {
+  loadingScreen.innerHTML = loadingTexts[currentTextIndex];
+  currentTextIndex = (currentTextIndex + 1) % loadingTexts.length;
+};
 
-const reflexion = regl({
-	cull: {
-		enable: true,
-		face: 'front'
-	},
-	uniforms: {
-		yScale: -1.0
-	}
-});
+setInterval(updateLoadingText, 500); // Cambia el texto cada medio segundo
 
-regl.frame(({
-	time
-}) => {
-	stats.begin();
-	fps.tick({
-		time
+// Creamos una función para cargar el contenido
+const loadContent = async () => {
+	return new Promise(resolve => {
+		
+		setTimeout(() => {
+			fps = require('./fps')(map, fovY);
+			resolve();
+		}, 2300); // Espera para la carga
+			
+		regl = require('regl')({
+			extensions: [
+				//'angle_instanced_arrays',
+				'OES_element_index_uint',
+				'OES_standard_derivatives'
+			],
+			optionalExtensions: [
+				//'oes_texture_float',
+				'EXT_texture_filter_anisotropic'
+			],
+			attributes: { alpha : false },
+		});
+			
+		map = require('./map')();
+		const mesh = require('./mesh');
+		drawMap = mesh(regl, map, useReflexion);
+		placement = require('./placement')(regl, map);
+		drawPainting = require('./painting')(regl);
 	});
-	placement.update(fps.pos, fps.fmouse[1], fovX());
-	regl.clear({
-		color: [0, 0, 0, 1],
-		depth: 1
-	});
-	context(() => {
-		if(useReflexion) {
-			reflexion(() => {
-				drawMap();
-				drawPainting(placement.batch());
-			});
+};
+
+const StartApp = async () => {
+	await loadContent();
+
+	document.body.removeChild(loadingScreen);
+	// Quitamos la pantalla de carga después de eso
+
+	const context = regl({
+		cull: {
+			enable: true,
+			face: 'back'
+		},
+		uniforms: {
+			view: fps.view,
+			proj: fps.proj,
+			yScale: 1.0
 		}
-		drawMap();
-		drawPainting(placement.batch());
 	});
-	stats.end();
-});
+	
+	const reflexion = regl({
+		cull: {
+			enable: true,
+			face: 'front'
+		},
+		uniforms: {
+			yScale: -1.0
+		}
+	});
+	
+	regl.frame(({
+		time
+	}) => {
+		stats.begin();
+		fps.tick({
+			time
+		});
+		placement.update(fps.pos, fps.fmouse[1], fovX());
+		regl.clear({
+			color: [0, 0, 0, 1],
+			depth: 1
+		});
+		context(() => {
+			if (useReflexion) {
+				reflexion(() => {
+					drawMap();
+					drawPainting(placement.batch());
+				});
+			}
+			drawMap();
+			drawPainting(placement.batch());
+		});
+		stats.end();
+	});
+};
+
+StartApp();
